bf94e89c1a27142e68d12fa52988c387625b5a29
Rodrigo Couto <r@rodg.co>
2022-04-06 17:41:14 -0300

    implement raw view

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,20 +7,28 @@
 	const writer = new HtmlRenderer({ smart: true });
 
 	let rendered = '';
+	let raw = false;
 
+	$: console.log(raw);
 	$: parsed = parser.parse(text);
 	$: rendered = writer.render(parsed);
 </script>
 
 <textarea bind:value={text} wrap="on" rows="10" placeholder="Type some markdown here..." />
+<label><input type="checkbox" bind:checked={raw} />Raw</label>
 
-<div id="preview">
-	{@html rendered || '<span class="placeholder">...it will render here.</span>'}
-</div>
+{#if raw}
+	<pre class="preview" class:placeholder={!rendered.length}>{rendered ||
+			'...it will render here.'}</pre>
+{:else}
+	<div class="preview" class:placeholder={!rendered.length}>
+		{@html rendered || '...it will render here.'}
+	</div>
+{/if}
 
 <style>
 	textarea,
-	div {
+	.preview {
 		background-color: hsl(0, 3%, 90%);
 		display: block;
 		overflow: auto;
@@ -32,12 +40,12 @@
 		border-radius: 5px;
 		outline: none;
 		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-		margin-bottom: 2rem;
+		margin: 0 0 2rem 0;
 	}
-	#preview :global(li) {
+	.preview :global(li) {
 		margin-left: 1.25rem;
 	}
-	#preview > :global(.placeholder) {
+	.preview.placeholder {
 		color: rgba(0, 0, 0, 0.5);
 	}
 </style>
