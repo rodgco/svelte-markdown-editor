3e6cfbb5e85787049f888f26213dd121698491b2
Rodrigo Couto <r@rodg.co>
2022-04-06 16:44:29 -0300

    automatize app name

--- src/routes/api/send_message.ts
+++ src/routes/api/send_message.ts
@@ -1,5 +1,10 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { supabase } from '$lib/supabase';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = process.env.SVELTE_APP_NAME;
 
 /** @type {import('./api/add_contact').RequstHandler} */
 export const post: RequestHandler = async ({ request }) => {
@@ -9,7 +14,7 @@ export const post: RequestHandler = async ({ request }) => {
 		.then((urlParams) => urlParams.entries())
 		.then((params) => Object.fromEntries(params));
 
-	const result = await supabase.from('Contacts').insert({ app: 'markdown-editor', ...contact });
+	const result = await supabase.from('Contacts').insert({ app, ...contact });
 
 	console.log(result);
 
